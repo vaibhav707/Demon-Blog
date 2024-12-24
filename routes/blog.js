13d6edc3ec69e3f8cb5b836cccfe0639bc721cf5
Vaibhav Bisht <vaibhavbisht707@gmail.com>
2024-12-24 19:36:07 +0530

    first-version

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,8 @@
 const {Router} = require("express");
 const multer = require("multer");
 const path = require("path");
-const Blog = require("../models/blog")
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -20,6 +21,25 @@ router.get("/add-new", (req, res) => {
     return res.render("addBlog", {
         user: req.user,
     });
+});
+
+router.get("/:id", async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy");
+    return res.render("blog", {
+        user: req.user,
+        blog,
+        comments,
+    });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+    const comment = await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage") , async (req, res) => {
