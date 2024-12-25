8c928805e23f39f8c5c3868f4fd21edde0e66dd3
Vaibhav Bisht <vaibhavbisht707@gmail.com>
2024-12-25 14:46:56 +0530

    Demon-version

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const router = Router();
 
 router.get("/signup", (req, res) => {
-    return res.render("signup");
+    return res.render("signup", {error: null});
 });
 
 router.get("/signin", (req, res) => {
@@ -12,14 +12,33 @@ router.get("/signin", (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-    const {fullName, email, password } = req.body;
-    await User.create({
-        fullName,
-        email,
-        password,
-    });
-    return res.redirect("/");
+    const { fullName, email, password } = req.body;
+
+    // Define a regex for a strong password
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8,}$/;
+
+    try {
+        if (!passwordRegex.test(password)) {
+            return res.render("signup", {
+                error: "Password must be at least 8 characters long, include uppercase, lowercase, a number, and a special character.",
+            });
+        }
+
+        const existingUser = await User.findOne({ where: { email } });
+        if (!existingUser) {
+            await User.create({ fullName, email, password });
+            return res.redirect("/user/signin");
+        }
+        return res.render("signup", {
+            error: "Email already in use",
+        });
+    } catch (error) {
+        return res.render("signup", {
+            error: "An unexpected error occurred. Please try again.",
+        });
+    }
 });
+
 
 router.post("/signin", async (req, res) => {
     try{
